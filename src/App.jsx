095a65db59b84d87eb76aca91022715dac5d8579
¨Melisa <meliochi@hotmail.com>
2026-06-09 19:07:25 -0300

    Seccion Activa

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         />
       </div>
       <div className="">
-        <Header />
+        <Header seccionActiva={seccionActiva} />
       </div>
       <div>
         <Footer />
