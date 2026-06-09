124d9585494c57a395b279d7fafad474c273ec96
Diego <diegperez04@gmail.com>
2026-06-09 20:00:43 -0300

    Avance en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import "./Home.css";
+
 function Home() {
   return (
     <div>
-      <div className="divuno">
-        <h1>PLATAFORMA DE RECICLAJE Y VOLUNTARIADO</h1>
+      <div className="divprincipal">
+        <div className="divizquierda">
+          <h1>PLATAFORMA DE RECICLAJE Y VOLUNTARIADO</h1>
+          <h2>Reciclar tiene recompensas reales</h2>
+          <p>
+            Juntá bucles reciclando y haciendo voluntariado. Canjeá descuntos y
+            beneficios para tu vida diaria mientras cuidas el planeta.
+          </p>
+          <button>Empezar a reciclar</button>
+          <button>Ver la tienda</button>
+        </div>
+        <div className="divderecha"></div>
       </div>
     </div>
   );
