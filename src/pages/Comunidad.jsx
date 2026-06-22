78f0d608283b1ff2325f143c7d93697aa6146ac1
trsh <ailumina.tech@gmail.com>
2026-06-22 20:16:44 -0300

    Revert "Agregar funcionalidad de canastas en Comunidad"
    This reverts commit 8381fd838a0528d76bbec9619a39621a25a88d38.

--- src/pages/Comunidad.jsx
+++ src/pages/Comunidad.jsx
@@ -1,38 +1,17 @@
-import Canastas from "../components/Canastas";
-import "./Comunidad.css";
-
 function Comunidad() {
   return (
     <div>
       <div className="divprincipal">
         <div className="divizquierda">
-          <h1 className="titulo-up">
-            Comunidad <em>Bucle</em>
-          </h1>
-          
-          <h2 className="subtitulo-comunidad">
-            Reciclar es más fácil y divertido en comunidad
-          </h2>
-          
-          <p className="descripcion-comunidad">
-            Comunícate, organízate y haz amigos mientras ayudas a resolver 
-            un problema real del medio ambiente.
+          <h1>COMUNIDAD BUCLE</h1>
+          <h2>Reciclar es mas facil y divertido en comunidad!</h2>
+          <p>
+            Comunicate organizate y haz amigos mientras ayudas a resolver un problema real
           </p>
-
-          <div className="botones-comunidad">
-            <button className="btn-comunidad-main">
-              Realizar un posteo en la comunidad
-            </button>
-            <button className="btn-comunidad-sec">
-              Ver posteos de la comunidad
-            </button>
-          </div>
-        </div>
-        <div className="divderecha">
+          <button>Realizar un posteo en la comunidad</button>
+          <button>Ver posteos de la comunidad</button>
         </div>
       </div>
-
-      <Canastas />
     </div>
   );
 }
